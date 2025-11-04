f49145f449a5bb2ac2eda63092c791d64bcf3515
kumarxanubhav <kumar24anubhav@gmail.com>
2025-11-04 11:39:15 +0530

    react

---
+++ Frontend/Anubhav/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Card from './component/Card'
+import Count1 from './component/Count1';
+
+function App() {
+  return (
+    <div>
+      <Count1></Count1>
+    </div>
+  );
+}
+
+export default App
---
+++ Frontend/Anubhav/src/component/Card.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import './Card.css'
+function Card() {
+  return (
+    <div id="Card">
+      <img src="" alt="image"></img>
+      <h1>Anubhav Kumar</h1>
+      <h2>12th Qualified</h2>
+      </div>
+
+    //   <div id="Navbar">
+
+    //   <a href="#home">Home</a>
+    //   <a href="#about">About</a>
+    //   <a href="#services">Services</a>
+    //   <a href="#contact">Contact</a>
+
+    // </div>
+  );
+}
+export default Card
